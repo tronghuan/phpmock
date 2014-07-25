5ba736c829c6b924d872b5c2ab13b954d082cc26
tronghuan <huandt92@gmail.com>
2014-07-25 16:51:26 +0700

    Them phan comment

--- application/modules/default/views/product/detailproduct.php
+++ application/modules/default/views/product/detailproduct.php
@@ -1,14 +1,26 @@
 <div class="center_content">
-	<div class="center_title_bar">My Cinema-U3000/DVBT, USB 2.0 TV BOX External, White</div>
+	<div class="center_title_bar">My Cinema-U3000/DVBT, USB 2.0 TV BOX
+		External, White</div>
 	<div class="prod_box_big">
 		<div class="top_prod_box_big"></div>
 		<div class="center_prod_box_big">
 			<div class="product_img_big">
-				<a href="javascript:popImage('http://localhost/phpmock/public/images/big_pic.jpg','Some Title')" title="header=[Zoom] body=[&nbsp;] fade=[on]" ><img src="http://localhost/phpmock/public/images/laptop.gif" alt="" border="0" /></a>
+				<a
+					href="javascript:popImage('http://localhost/phpmock/public/images/big_pic.jpg','Some Title')"
+					title="header=[Zoom] body=[&nbsp;] fade=[on]"><img
+					src="http://localhost/phpmock/public/images/laptop.gif" alt=""
+					border="0" /></a>
 				<div class="thumbs">
-					<a href="http://www.free-css.com/" title="header=[Thumb1] body=[&nbsp;] fade=[on]"><img src="http://localhost/phpmock/public/images/thumb1.gif" alt="" border="0" /></a> 
-					<a href="http://www.free-css.com/" title="header=[Thumb2] body=[&nbsp;] fade=[on]"><img src="http://localhost/phpmock/public/images/thumb1.gif" alt="" border="0" /></a>
-					<a href="http://www.free-css.com/" title="header=[Thumb3] body=[&nbsp;] fade=[on]"><img src="http://localhost/phpmock/public/images/thumb1.gif" alt="" border="0" /></a>
+					<a href="http://www.free-css.com/"
+						title="header=[Thumb1] body=[&nbsp;] fade=[on]"><img
+						src="http://localhost/phpmock/public/images/thumb1.gif" alt=""
+						border="0" /></a> <a href="http://www.free-css.com/"
+						title="header=[Thumb2] body=[&nbsp;] fade=[on]"><img
+						src="http://localhost/phpmock/public/images/thumb1.gif" alt=""
+						border="0" /></a> <a href="http://www.free-css.com/"
+						title="header=[Thumb3] body=[&nbsp;] fade=[on]"><img
+						src="http://localhost/phpmock/public/images/thumb1.gif" alt=""
+						border="0" /></a>
 				</div>
 			</div>
 			<div class="details_big_box">
@@ -27,5 +39,17 @@
 			</div>
 		</div>
 		<div class="bottom_prod_box_big"></div>
+	</div>
+	<div id="comment">
+		<form method="post" action="http://">
+			<label for="name">Name</label>
+			<input type="text" name="name" id="name" placeholder="Nhập tên của bạn vào" /><br />
+			<label for="email">Email</label>
+			<input type="text" name="email" id="email" placeholder="Nhập địa chỉ email vào" /><br/>
+			<textarea name="comments" cols="50" rows="10" class="html-text-box">Enter your comments here...</textarea>
+			<br>
+			<input type="submit" value="Submit" class="html-text-box">
+			<input type="reset" value="Reset" class="html-text-box">
+		</form>
 	</div>
 </div>
